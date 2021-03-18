3de3e1f7cd216461a4c45970ccebf1ffb04e74d3
Alessandro Dutra de Andrade <cachopaweb@gmail.com>
2021-03-18 13:32:30 -0400

    Refactored to automatically identify file types

--- src/Horse.ServerStatic.pas
+++ src/Horse.ServerStatic.pas
@@ -13,38 +13,9 @@ procedure Middleware(Req: THorseRequest; Res: THorseResponse; Next: TProc);
 
 implementation
 
+uses System.Net.Mime;
+
 var Path: string;
-
-function StrInArray(Str: String; const Lista: Array of string): Boolean;
-var
-  i: integer;
-begin
-  for i := Low(Lista) to High(Lista) do
-  begin
-    if Lista[i] = Str then
-    begin
-      Result := True;
-      Exit;
-    end;
-  end;
-  Result := False;
-end;
-
-function ChangeContentType(Extension: String): String;
-begin
-  if StrInArray(Extension, ['.jpg', '.png', '.jpeg']) then
-    Result := 'image/png';
-  if StrInArray(Extension, ['.html', '.htm']) then
-    Result := 'text/html; charset=utf-8';
-  if StrInArray(Extension, ['.js']) then
-    Result := 'application/javascript';
-  if StrInArray(Extension, ['.css']) then
-    Result := 'text/css';
-  if StrInArray(Extension, ['.json']) then
-    Result := 'application/json';
-  if Result = '' then
-    Result := 'text/html; charset=utf-8';
-end;
 
 function ServerStatic(PathRoot: String): THorseCallback;
 begin
@@ -57,6 +28,8 @@ var
   FileStream: TFileStream;
   PathFull: string;
   Extension: string;
+  aType: string;
+  aKind: TMimeTypes.TKind;
 begin
   Extension := ExtractFileExt(Req.RawWebRequest.PathInfo);
   if not Extension.isEmpty then
@@ -71,7 +44,8 @@ begin
       FileStream.Position := 0;
       //send response
       Res.Status(THTTPStatus.OK);
-      Res.RawWebResponse.SetCustomHeader('Content-Type', ChangeContentType(Extension));
+      TMimeTypes.Default.GetFileInfo(PathFull, aType, aKind);
+      Res.RawWebResponse.ContentType := aType;
       Res.RawWebResponse.ContentStream := FileStream;
       Res.RawWebResponse.SendResponse;
     finally
